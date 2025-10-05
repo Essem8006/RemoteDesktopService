6817995b3c7a0eb2a6a679de5a77ba984378e8fe
Essem <149950536+Essem8006@users.noreply.github.com>
2025-10-05 13:29:27 +0100

    Screen capture works

---
+++ Phase1/pythonInput/main.py
@@ -0,0 +1,5 @@
+import pyautogui
+screen_size = pyautogui.size()
+pyautogui.moveTo(100, 100, duration = 1)
+pyautogui.moveRel(0, 50, duration = 1)
+#https://www.geeksforgeeks.org/python/mouse-keyboard-automation-using-python/
